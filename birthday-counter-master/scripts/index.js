e17d64535d01a0778fdc2567de6fe185eda31257
corgi4everyone <71846916+corgi4everyone@users.noreply.github.com>
2020-10-24 02:06:05 +0630

    Update index.js

--- birthday-counter-master/scripts/index.js
+++ birthday-counter-master/scripts/index.js
@@ -26,7 +26,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("October 25,2020 00:00:00").getTime();
+let countDown = new Date("October 24,2020 02:10:00").getTime();
 x = setInterval(function() {
   let now = new Date().getTime(),
     distance = countDown - now;
