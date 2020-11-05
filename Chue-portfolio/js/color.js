77a9baf0a70d9b91fe86de89d83fadcf96267c99
corgi4everyone <71846916+corgi4everyone@users.noreply.github.com>
2020-11-06 03:26:33 +0630

    Add files via upload

---
+++ Chue-portfolio/js/color.js
@@ -0,0 +1,10 @@
+
+function chgBgColor() {
+    var homebgColor=prompt("Enter color code to change home section background color");
+    var aboutbgColor=prompt("Enter color code to change about section background color")
+    var edubgColor=prompt("Enter color code to change about section background color")
+    document.getElementById("home").style.background=homebgColor;
+    document.getElementById("about").style.background=aboutbgColor;
+    document.getElementById("education").style.background=edubgColor;
+}
+
