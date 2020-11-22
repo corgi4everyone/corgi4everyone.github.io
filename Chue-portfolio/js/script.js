5bb9675aa40b9bfa89331a16162c051ca111ba20
corgi4everyone <71846916+corgi4everyone@users.noreply.github.com>
2020-11-23 04:30:15 +0630

    Update script.js

--- Chue-portfolio/js/script.js
+++ Chue-portfolio/js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
   $(window).scroll(function(){
     if(this.scrollY>20){
@@ -5,13 +6,35 @@ $(document).ready(function(){
     }else{
       $(".navbar").removeClass("sticky");
     }
+    if(this.scrollY>500){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
     
+  });
+
+
+  // slide-up script
+  $('.scroll-up-btn').click(function() {
+      $('html').animate({scrollTop:0});
   });
   // toggle menu/navbar
   $('.menu-btn').click(function() {
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
   });
+
+  // typing animation script
+
+  var options = {
+      strings: ["Student","Volunteer","Financial Executive","Project Manager"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  };
+  var typed =new Typed('.typing',options)
+
   // owl carousel script
   $('.carousel').owlCarousel({
     margin: 20,
